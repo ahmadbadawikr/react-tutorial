489aedef745f12569fcfef4bc1aa4e9a3094d3ca
Ahmad Badawi Kasyfur Rahman <ahmad.badawi.k@mail.ugm.ac.id>
2024-07-18 19:05:52 +0700

    conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,71 @@
 // import { UserProfile } from "./components/UserProfile"
-
-import { UserDetails } from "./components/UserDetails"
+// import { UserDetails } from "./components/UserDetails"
 
 export default function App() {
-  const mockUsers = [
-    {
-      id:1,
-      username:"Aegon",
-      email: "aegon@kingslanding.com"
-    },
-    {
-      id:2,
-      username:"Maegor",
-      email: "maegor@kingslanding.com"
-    },
-    {
-      id:3,
-      username:"Aenys",
-      email: "aenys@kingslanding.com"
-    },
-    {
-      id:4,
-      username:"Jaehaerys",
-      email: "jaehaerys@kingslanding.com"
-    }
-  ]
+  const USER_STATUS = "ACCOUNT DISABLED";
 
-  return (
-    <div>
-      {mockUsers.map(
-        (user) => {
-          return <UserDetails key={user.id} user={user}/>
-        }
-      )}
-    </div>)
+  switch(USER_STATUS) {
+    case "NOT VERIFIED":
+      return (
+        <div>
+          <span>
+            You are not verified. Please verify your Email or Mobile Number.
+          </span>
+        </div>
+      );
+    case "VERIFIED":
+      return(
+        <div>
+          <span>
+            You are verified. Click here to access the dashboard.
+          </span>
+        </div>
+      );
+    case "ACCOUNT DISABLED":
+      return(
+        <div>
+          <span>
+            Your account is disabled.
+          </span>
+        </div>
+      )
+
+  }
+
 }
 
+
+  // const mockUsers = [
+  //   {
+  //     id:1,
+  //     username:"Aegon",
+  //     email: "aegon@kingslanding.com"
+  //   },
+  //   {
+  //     id:2,
+  //     username:"Maegor",
+  //     email: "maegor@kingslanding.com"
+  //   },
+  //   {
+  //     id:3,
+  //     username:"Aenys",
+  //     email: "aenys@kingslanding.com"
+  //   },
+  //   {
+  //     id:4,
+  //     username:"Jaehaerys",
+  //     email: "jaehaerys@kingslanding.com"
+  //   }
+  // ]
+
+  // return (
+  //   <div>
+  //     {mockUsers.map(
+  //       (user) => {
+  //         return <UserDetails key={user.id} user={user}/>
+  //       }
+  //     )}
+  //   </div>)
 
 
 
