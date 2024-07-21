963e564fe4c59e2c4651ededd7cf103a92aed877
Ahmad Badawi Kasyfur Rahman <ahmad.badawi.k@mail.ugm.ac.id>
2024-07-21 16:34:56 +0700

    lear useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -2,42 +2,86 @@
 // import { UserDetails } from "./components/UserDetails"
 // import { LoginForm } from "./components/LoginForm"
 // import { RegisterForm } from "./components/RegisterForm"
+// import { UserDetails } from "./components/UserDetails";
 
-import { useState } from "react";
-import { UserDetails } from "./components/UserDetails";
+import { useState, useEffect } from "react";
 
 export default function App() {
-  const [users, setUsers] = useState([
-    {
-          id:1,
-          username:"Aegon",
-          email: "aegon@kingslanding.com"
-        },
-        {
-          id:2,
-          username:"Maegor",
-          email: "maegor@kingslanding.com"
-        },
-        {
-          id:3,
-          username:"Aenys",
-          email: "aenys@kingslanding.com"
-        },
-        {
-          id:4,
-          username:"Jaehaerys",
-          email: "jaehaerys@kingslanding.com"
-        }
-  ]);
-
-
-  return <div>
-    {users.map(
-      (user) => 
-    <UserDetails key={user.id} user={user} setUsers={setUsers}/>
-    )}
+ const [counter, setCounter] = useState(0);
+ const [sync, setSync] = useState(false);
+ 
+ useEffect(()=> {
+  console.log("rendering...")
+  document.title = "React Tutorial"+ counter
+ }, [sync])
+ return (
+  <div>
+    <div>
+      You clicked the button {counter} times
+    </div>
+    <button onClick={()=> setCounter((count)=> count+1)}>Click Me</button>
+    <button onClick={()=> setSync((current)=> !current)}>Sync</button>
   </div>
+ )
 }
+
+// const [username, setUsername] = useState("")
+//   const [email, setEmail] = useState("")
+//   const [users, setUsers] = useState([
+//     {
+//           id:1,
+//           username:"Aegon",
+//           email: "aegon@kingslanding.com"
+//         },
+//         {
+//           id:2,
+//           username:"Maegor",
+//           email: "maegor@kingslanding.com"
+//         },
+//         {
+//           id:3,
+//           username:"Aenys",
+//           email: "aenys@kingslanding.com"
+//         },
+//         {
+//           id:4,
+//           username:"Jaehaerys",
+//           email: "jaehaerys@kingslanding.com"
+//         }
+//   ]);
+
+
+  // return (
+  // <div>
+  //   <form onSubmit={(e)=> {
+  //     e.preventDefault()
+  //     console.log(username, email)
+  //   }}>
+  //     <div>
+  //       <label htmlFor="username"> Username</label>
+  //       <input name="username" id="username" value={username} onChange={(e)=> {
+  //         setUsername(e.target.value)
+  //       }}/>
+  //     </div>
+  //     <div>
+  //       <label htmlFor="email"> Email</label>
+  //       <input name="email" id="email" value={email} onChange={(e)=> {
+  //         setEmail(e.target.value)
+  //       }}/>
+  //     </div>
+  //     <button>Add User</button>
+  //   </form>
+  //   <br />
+  //   {users.map(
+  //     (user) => 
+  //       <UserDetails 
+  //         key={user.id} 
+  //         user={user} 
+  //         setUsers={setUsers}/>
+  //   )}
+    
+  // </div>)
+
     // <div>
     //   <RegisterForm></RegisterForm>
     // </div>
